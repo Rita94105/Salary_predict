88f8589e28b695e177b3b47a99422085a881903f
will_chen <willchen356@gmail.com>
2024-05-23 21:12:57 +0800

    update preprocessing_v2, split data by salary_in_usd 150000

--- preprocessing_v2.R
+++ preprocessing_v2.R
@@ -14,3 +14,9 @@ for (col in cols_encoding) {
 # k-means
 data$salary_in_usd_cluster <- do_kmeans(data, 10)
 colnames(data)
+
+# split data by salary_in_usd 150000
+high_salary_data <- subset(data, salary_in_usd > 150000)
+low_salary_data <- subset(data, salary_in_usd <= 150000)
+nrow(high_salary_data)
+nrow(low_salary_data)
