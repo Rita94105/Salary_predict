e8e058686d684f7497b30169d7467c24dd141a90
Rita <rita@RitadeMacBook-Pro.local>
2024-05-24 16:39:53 +0800

    add footer

--- code/ui.R
+++ code/ui.R
@@ -4,6 +4,14 @@ library(bsicons)
 
 source("ui_data.R")
 
+footer_ui <- function() {
+  div(class = "footer",
+      style = "padding: 10px; text-align: center; ;",
+      p("The project is led by ",
+        a(href = "https://www.changlabtw.com/pi-243732347837528.html",target = "_blank", "Dr. Chang"),
+        " in 2024"))
+}
+
 ui <- navbarPage(
   title = div(img(src="cs.png", height = "30px", weight = "30px"), "Salary Prediction 2024", style = "padding-top: 30px; padding-right: 20px;"),
   theme = bs_theme(version = 5, bootswatch = "minty"),
@@ -21,5 +29,6 @@ ui <- navbarPage(
   navbarMenu(
     title = "Prediction",
     tabPanel("Salary Prediction", "Tab content 6")
-  )
+  ),
+  footer_ui()
 )
